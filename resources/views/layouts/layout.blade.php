e4fc5c264f5a797c989efdff4491b26541341bf2
Kevin Schaefer <kevin.schaefer13@gmail.com>
2018-05-22 21:08:37 +0200

    added bruger menu for smartphone and removed fixed-top

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,5 +1,5 @@
 </<!doctype html>
-<html class="has-navbar-fixed-top">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,11 +1,16 @@
-<nav class="navbar is-fixed-top is-dark" role="navigation" aria-label="main navigation">
+<nav class="navbar is-dark" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
         <div class="navbar-item">
         <h5 class="title is-5 has-text-white is-italic">Sandras & Thommys Hochzeitfotos</h5>
             
         </div>
+        <div class="navbar-burger burger" data-target="navbar">
+                <span></span>
+                <span></span>
+                <span></span>
+        </div>
     </div>
-    <div class="navbar-menu">
+    <div class="navbar-menu" id="navbar">
             <div class="navbar-start">
                 <a class="navbar-item" href="/photographs">
                     Fotografin
