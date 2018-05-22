f6665570e1263f8df80123af4127df74be584f67
Kevin Schaefer <kevin.schaefer13@gmail.com>
2018-05-22 18:26:56 +0200

    welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.layout')
 @section('content')
-<p> test </p>
+</br>
+<div class="content is-clearfix">
+    <h1>Liebes Brautpaar, liebe Gäste</h1>
+    <h3>Herzlich Willkommen in der Fotogalerie von der Hochzeit von Sandra & Thommy</h3>
+    <p>Es gibt drei Seiten: Fotografin, Gäste und Einwegkameras</p>
+    <ul>
+    <li> Fotografin - Hier sind die Bilder hochgeladen, die von der Fotografin geschossen wurden</li>
+    <li> Gäste - Hier habt ihr die Möglichkeit selber Bilder hochzuladen und ihr könnt Bilder von anderen Gästen anschauen </li>
+    <ul>
+        <li>Das Hochladen von mehreren Bildern kann einige Minuten dauern, bitte währendessen die Seite nicht neu laden</li>
+    </ul>
+    <li> Einwegkameras - Hier sind die Bilder hochgeladen, welche mit den Einwegkameras von euch geschossen wurden</li>
+    </ul>
+    <br>
+    <p>Viel Spass</p>
+    <br>
+    <p><strong>Hinweis: </strong>Um ein Bild in der original Grafik herunterzuladen, 
+    müsst ihr auf das Bild klicken, danach mit einem Rechtsklick auf das vergrösserte Bild klicken und <strong>Grafik speichern unter auswählen</strong></p>
+    
+
+</div>
+
 
 @endsection
